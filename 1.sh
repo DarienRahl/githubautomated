257b24ffcee09fb88735bc8b4f773eb8acad0ad3
DarienRahl <36547448+DarienRahl@users.noreply.github.com>
2024-10-13 11:25:09 +0200

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,11 @@
+The GitHub Advisory Database now features the Exploit Prediction Scoring System (EPSS) from the global Forum of Incident Response and Security Teams (FIRST), helping you better assess vulnerability risks.
+
+EPSS scores predict the likelihood of a vulnerability being exploited, with scores ranging from 0 to 1 (0 to 100%). Higher scores mean higher risk. We also show the EPSS score percentile, indicating how a vulnerability compares to others.
+
+For example, a 90.534% EPSS score at the 95th percentile means:
+
+90.534% chance of exploitation in the next 30 days.
+95% of other vulnerabilities are less likely to be exploited.
+Learn more in the FIRST’s EPSS User Guide.
+
+This feature will be available in GitHub Enterprise Server version 3.16 and later.
